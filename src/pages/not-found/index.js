e59241ebfcd2e7f49011920a6b2e72c201a07146
Lana Souza <laniziamunique@gmail.com>
2021-08-25 14:54:26 -0300

    criação da página de not-found

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -4,21 +4,20 @@ const createPage = () => {
   const rootElement = document.createElement('div');
   const contentnewElement = `
   <section class="not-found-container">
-  <div class="not-found-glass">
-    <div class="not-found-content">
-     <img class="not-found-logo" src="./img/Amitié2.png" alt="">
-      <h1 class="not-found-title">Ops! Página não encontrada.</h1>
-      <a class="go-back-feed">Clique aqui para ir para o feed!</a>
-      <img src="./img/undraw_happy_women_day_fbjt.svg" class='image-not-found' alt=''/>
-    </div>
-  </div>
+    <div class="not-found-page">
+      <img src="../../img/code-girls.svg" alt="" class="logo">
+      <img class="img-error-404" src="../../img/404 PAGE NOT FOUND.svg" alt="">
+      <h1 class="not-found-title">Ops! Página não encontrada</h1>
+      <p class="not-found-text">Desculpe, não encontramos a página que você buscou! Clique <a class="not-found-link-home" id="go-back">AQUI</a> para retornar.</p>
+      <button class="back-to"></button>
+</div>
 </section>
   `;
   // registerListener
   rootElement.innerHTML = contentnewElement;
 
-  const goBackButton = rootElement.querySelector('.go-back-feed');
-  goBackButton.addEventListener('click', () => {
+  const goBackLink = rootElement.querySelector('#go-back');
+  goBackLink.addEventListener('click', () => {
     changePage('/');
   });
 
