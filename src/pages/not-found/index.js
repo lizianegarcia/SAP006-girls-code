06cd71d93722a27cb960274fe4b8ed8ef5ed5423
Lana Souza <laniziamunique@gmail.com>
2021-08-25 15:23:34 -0300

    identação do código

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -4,14 +4,14 @@ const createPage = () => {
   const rootElement = document.createElement('div');
   const contentnewElement = `
   <section class="not-found-container">
-    <div class="not-found-page">
-      <img src="../../img/code-girls.svg" alt="" class="logo">
-      <img class="img-error-404" src="../../img/404 PAGE NOT FOUND.svg" alt="">
-      <h1 class="not-found-title">Ops! Página não encontrada</h1>
-      <p class="not-found-text">Desculpe, não encontramos a página que você buscou! Clique <a class="not-found-link-home" id="go-back">AQUI</a> para retornar.</p>
-      <button class="back-to"></button>
-</div>
-</section>
+      <div class="not-found-page">
+        <img src="../../img/code-girls.svg" alt="" class="logo">
+        <img class="img-error-404" src="../../img/404 PAGE NOT FOUND.svg" alt="">
+        <h1 class="not-found-title">Ops! Página não encontrada</h1>
+        <p class="not-found-text">Desculpe, não encontramos a página que você buscou! Clique <a class="not-found-link-home" id="go-back">AQUI</a> para retornar.</p>
+        <button class="back-to"></button>
+      GI</div>
+  </section>
   `;
   // registerListener
   rootElement.innerHTML = contentnewElement;
